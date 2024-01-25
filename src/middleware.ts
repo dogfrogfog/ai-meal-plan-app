e4d0c3a2fcf84d799af849d14631a29d28d12636
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-01-25 21:40:30 +0100

    add /api/stripe/checkout_sessions to public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/stripe/webhook", "/api/clerk/webhook"],
+  publicRoutes: [
+    "/api/stripe/webhook",
+    "/api/stripe/checkout_sessions",
+    "/api/clerk/webhook",
+  ],
 });
 
 export const config = {
